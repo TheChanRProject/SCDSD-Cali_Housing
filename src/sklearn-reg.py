ab3d80421af48ea8cb407dd81db9fa5fc4e74ddf
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-23 23:56:53 -0700

    Adding edits

---
+++ src/sklearn-reg.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pyplot
+import seaborn as sns
+from jupyterthemes import jtplot
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_square_error
