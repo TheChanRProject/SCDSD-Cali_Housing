3416fadb8333ba90198319c2f488db45cafecf52
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-24 00:18:30 -0700

    Adding edits

--- src/sklearn-reg.py
+++ src/sklearn-reg.py
@@ -21,8 +21,10 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size= 0.2, random_
 
 model = LinearRegression()
 model.fit(X_train,Y_train)
-
 y_train_pred = model.predict(X_train)
+
+
+# Regression Assessment 
 rmse = np.sqrt(mean_squared_error(Y_train, y_train_pred))
 r2 = round(model.score(X_train, Y_train), 2)
 
