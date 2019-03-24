9acd2d3864751a7cf5ea448e5778ee1335d2e611
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-23 23:59:40 -0700

    Adding edits

--- src/sklearn-reg.py
+++ src/sklearn-reg.py
@@ -6,3 +6,9 @@ from jupyterthemes import jtplot
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv("data/reg_demo_data.csv")
+data.head()
+print(list(data.columns))
+data.drop(list(data.columns)[0], axis=1, inplace=True)
+data.head()
